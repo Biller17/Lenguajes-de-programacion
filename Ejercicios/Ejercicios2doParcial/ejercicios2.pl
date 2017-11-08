ef0617c314d2366adfc12ef8a1ee9fbf6f008248
Biller <abiller95@gmail.com>
2017-11-08 13:31:32 -0600

    -

--- Ejercicios/Ejercicios2doParcial/ejercicios2.pl
+++ Ejercicios/Ejercicios2doParcial/ejercicios2.pl
@@ -3,12 +3,10 @@
 
 /* Ejercicio Adrian Biller A010128940*/
 
-islist(X):-  var(X), !, fail.
-islist([]).
-islist([_|T]):- islist(T).
 
-divisionLista([], REF, MIN, MAX):- write(MIN), write(MAX).
-divisionLista([H|T],REF, MIN, MAX):- H > REF, append([], MAX, X) ,append([H], X, Z), divisionLista(T,REF,MIN,Z).
-divisionLista([H|T],REF, MIN, MAX):- H < REF, append([], MIN, X) ,append([H], X, Z), divisionLista(T,REF,Z,MAX).
-divisionLista([H|T],REF, MIN, MAX):- H =:= REF, divisionLista(T,REF,MIN,MAX).
-divisionLista(LIST, REF, MIN, MAX):- islist(MIN), islist(MAX), divisionLista(LIST, REF, [], [])
+
+
+divisionLista([], REF, MIN, MAX, MIN,MAX).
+divisionLista([H|T],REF, MIN, MAX, AMIN,AMAX):- H > REF, append([H], AMAX, Z), divisionLista(T,REF,MIN,MAX,AMIN,Z).
+divisionLista([H|T],REF, MIN, MAX, AMIN,AMAX):- H < REF, append([H], AMIN, Z), divisionLista(T,REF,MIN,MAX,Z,AMAX).
+divisionLista([H|T],REF, MIN, MAX, AMIN,AMAX):- H =:= REF, divisionLista(T,REF,MIN,MAX).
